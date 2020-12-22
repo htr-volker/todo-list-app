ece3d85410782120a8e5979e97a08d664b5d7e98
htr-volker <htr.volker@gmail.com>
2020-12-22 10:04:59 +0000

    fixing destination folder

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y python3-pip python3-venv unzip chromium-browser
 
 # Install chromedriver
 wget -P /home/jenkins/chromedriver https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
-unzip /home/jenkins/chromedriver/chromedriver_linux64.zip
+unzip /home/jenkins/chromedriver/chromedriver_linux64.zip -d /home/jenkins/chromedriver
 
 # Create/activate Python virtual environment
 python3 -m venv venv
